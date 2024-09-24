c03d5f707e66832fefb738a576ff5cbd5bf79633
Taylor Lo <taylorloph@gmail.com>
2024-09-24 18:31:29 -0400

    Path Converters

--- challenges/views.py
+++ challenges/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
+
 def monthly_challenge(request, month):
     challenge_text = None
     if month == 'january':
