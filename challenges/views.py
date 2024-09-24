8ae08bd15de309bd4a411f0ab4c2e67ba2543e7a
Taylor Lo <taylorloph@gmail.com>
2024-09-24 17:48:10 -0400

    Adding More Views & URLs

--- challenges/views.py
+++ challenges/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
+def january(request):
     return HttpResponse("This works!")
+
+def february(request):
+    return HttpResponse("Walk for at least 20 minutes every day!")
